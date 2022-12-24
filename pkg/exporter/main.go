f0a5e4e0b33b6744e3ce270a1a4c847239b7f157
Madiyor <abdukhashimov@yandex.ru>
2022-12-24 21:30:02 +0500

    add exporter basic function

--- pkg/exporter/main.go
+++ pkg/exporter/main.go
@@ -1,4 +1,23 @@
 package exporter
 
+import "github.com/jmoiron/sqlx"
+
 type Exporter interface {
+	Export(tableName string) error
+}
+
+type Export struct {
+	psqlConn   *sqlx.DB
+	cHouseConn *sqlx.DB
+}
+
+func New(psqlConn, cHouseConn *sqlx.DB) Exporter {
+	return &Export{
+		psqlConn:   psqlConn,
+		cHouseConn: cHouseConn,
+	}
+}
+
+func (e *Export) Export(tableName string) error {
+	return nil
 }
