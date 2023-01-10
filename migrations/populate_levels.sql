d8b319ff31b3c92b30166fbcd1638d88c75b4a39
madiyor-abdukhashimov <madiyor.abdukhashimov@global-fashion-group.com>
2023-01-10 09:11:25 +0500

    add migrations

---
+++ migrations/populate_levels.sql
@@ -0,0 +1,5 @@
+-- insert data
+insert into levels(id, name, balls, created_at, updated_at) values (1, '{"en": "1", "ru": "1", "uz": "1"}', 60000, now(), now());
+insert into levels(id, name, balls, created_at, updated_at) values (2, '{"en": "2", "ru": "2", "uz": "2"}', 20000, now(), now());
+insert into levels(id, name, balls, created_at, updated_at) values (3, '{"en": "3", "ru": "3", "uz": "3"}', 120000, now(), now());
+insert into levels(id, name, balls, created_at, updated_at) values (4, '{"en": "4", "ru": "4", "uz": "4"}', 150000, now(), now());
---
+++ migrations/populate_transactions.sql
@@ -0,0 +1,17 @@
+INSERT INTO transactions(
+        id,
+        user_id,
+        balls,
+        level_id,
+        step,
+        updated_at,
+        created_at
+    )
+SELECT i,
+    i,
+    300,
+    1,
+    1,
+    now(),
+    now()
+FROM generate_series(1, 1000000) s(i)
